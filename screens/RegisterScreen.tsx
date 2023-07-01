7504755caa762dce5eac653aff502fd7e1e68ea8
Abhishek Kumar <abhishekverman3459@gmail.com>
2023-07-01 20:55:10 +0530

    Adding the API_URL with backend port

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -23,7 +23,7 @@ import { Alert } from "react-native";
 
 WebBrowser.maybeCompleteAuthSession();
 // genral format "http://ipaddress of expo like 192..."+:backend port
-const API_URL = "http://192.168.146.201:5000";
+const API_URL = "http://192.168.43.163:5000";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Register">;
 
